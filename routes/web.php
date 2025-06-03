3d5af48a3026edb14315a95730bf1f2403b1317b
alexrv98 <libravarillas777@gmail.com>
2025-06-03 16:40:10 -0600

    Primer commit

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+/* Nuevo dashboard, en la carpeta “dasboard” */
+Route::get('/alt-dashboard', function () {
+    return view('dashboard.index');           // resources/views/dasboard/index.blade.php
+})->middleware(['auth', 'verified'])
+  ->name('dashboard.alt');  
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
