21a0910009e57ea61db5e5b1376f8eea64f70829
alexrv98 <libravarillas777@gmail.com>
2025-07-06 17:27:11 -0600

    cambios

--- resources/views/dashboard/partials/scripts.blade.php
+++ resources/views/dashboard/partials/scripts.blade.php
@@ -4,6 +4,7 @@
     new Chart(estadoClientesCtx, {
         type: 'doughnut',
         data: {
+            
             labels: ['Pagados', 'Con Deuda', 'Pendientes Hoy'],
             datasets: [{
                 label: 'Estado de clientes',
@@ -22,6 +23,8 @@
             }]
         },
         options: {
+                animation: false,
+
             responsive: true,
             cutout: '60%',
             plugins: {
@@ -53,6 +56,8 @@
             }]
         },
         options: {
+                            animation: false,
+
             responsive: true,
             scales: {
                 y: {
@@ -88,26 +93,3 @@
         }
     });
 </script>
-@push('scripts')
-    <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
-    <script>
-        const opcionesDataTable = {
-            pageLength: 10,
-            language: {
-                url: '//cdn.datatables.net/plug-ins/1.13.6/i18n/es-MX.json'
-            }
-        };
-
-        $(document).ready(function () {
-            const tablaClientesHoy = $('#tabla-clientes-hoy');
-            if (tablaClientesHoy.length) {
-                tablaClientesHoy.DataTable(opcionesDataTable);
-            }
-
-            const tablaClientesAtrasados = $('#tabla-clientes-atrasados');
-            if (tablaClientesAtrasados.length) {
-                tablaClientesAtrasados.DataTable(opcionesDataTable);
-            }
-        });
-    </script>
-@endpush
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-
+        <link rel="icon" href="{{ asset('favicon.ico') }}" sizes="any">
+        
+        
         <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
@@ -14,7 +16,7 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
-        
+
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
